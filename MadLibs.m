337309c322c1f01ad74ed3550bc1d7f4590c31fe
Sandra <82436702+Sandra1me@users.noreply.github.com>
2022-07-20 09:48:00 +0200

    Add files via upload

---
+++ MadLibs.m
@@ -0,0 +1,14 @@
+animal1=input("Animal: ",'s');
+adjective1=input("Adjective: ",'s');
+adjective2=input("Adjective: ",'s');
+adjective3=input("Adjective: ",'s');
+noun1=input("Noun: ",'s');
+noun2=input("Noun: ",'s');
+
+fprintf("\nA PEANUT AND HIS POOCH\n")
+fprintf("If you ever met my %s, Snoopy, then you know he's not your \n", animal1)
+fprintf("average canine companion. Some kids might find it %s \n", adjective1)
+fprintf("that their beagle has such a/an %s imagination, but not me! \n",adjective2)
+fprintf("All I've ever wanted was a normal, %s dog. Why can't I have \n",adjective3)
+fprintf("a/an %s just like everyone else? Instead, I've got a part-time pet,\n",noun1)
+fprintf("part-time World War pilot, part time %s, and a full-time pain!\n",noun2)
